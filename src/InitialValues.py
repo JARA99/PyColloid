f424a39a6c437099d8e74bb062404ebc713770c0
Jorge Alejandro <jorgealejandro1999@gmail.com>
2021-11-19 15:56:28 -0600

    Initial values change

--- src/InitialValues.py
+++ src/InitialValues.py
@@ -57,7 +57,7 @@ def ini_values(i,m,q,x,y,vx,vy,ax,ay,Lista):   # Función para asignar valores r
 
 def export(Lista,nombre,N):   # Función para crear un archivo .csv con toda la información de las particulas dadas
     a = str(nombre)+'.csv'
-    f = open(a, 'w')
+    f = open(a, 'w', newline='')
     w = csv.writer(f)
     w.writerow(["Id", "m", "q","rx","ry","vx","vy","ax","ay"])  # Encabezado
     w.writerow([])
