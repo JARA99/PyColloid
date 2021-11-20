9c177d00173f244ede4a58ff41b84546b1015d1c
Jorge Alejandro <jorgealejandro1999@gmail.com>
2021-11-20 16:38:31 -0600

    Easier

--- src/main.py
+++ src/main.py
@@ -62,10 +62,8 @@ with open('../Parameters.txt','r') as file:
     MinX = 0
     MaxY = float(file.readline().replace('MaxY: ',''))
     MinY = 0
-    MaxVx = float(file.readline().replace('MaxVx: ',''))
-    MinVx = float(file.readline().replace('MinVx: ',''))
-    MaxVy = float(file.readline().replace('MaxVy: ',''))
-    MinVy = float(file.readline().replace('MinVy: ',''))
+    MaxV = float(file.readline().replace('MaxV: ',''))
+    MinV = float(file.readline().replace('MinV: ',''))
     ForceConstant = float(file.readline().replace('ForceConstant: ',''))
     DeltaTime = float(file.readline().replace('DeltaTime: ',''))
     Iterations = int(file.readline().replace('Iterations: ',''))
@@ -85,8 +83,8 @@ r_q =[MinCharge,MaxCharge]    # rango carga
 r_r =[MinRadius,MaxRadius]    # rango del radio
 r_x =[MinX,MaxX]              # rango posición x
 r_y =[MinY,MaxY]              # rango posición y
-r_vx=[MinVx,MaxVx]             # rango velocidad x
-r_vy=[MinVy,MaxVy]             # rango velocidad y
+r_vx=[MinV,MaxV]             # rango velocidad x
+r_vy=[MinV,MaxV]             # rango velocidad y
 
 cicles = Iterations
 
